5f7847de67952766da822fb117802b2372e3dd48
DrillND <81124904+DrillND@users.noreply.github.com>
2021-04-27 09:26:17 +0900

    Add files via upload

---
+++ 20210427 R 문제 제출/20210426 R 문제3.R
@@ -0,0 +1,20 @@
+d1 <- 1:50
+d2 <- 51:100
+d1
+d2
+d1+d2
+d2-d1
+d1*d2
+max(d2)
+min(d2)
+mean(d1)
+mean(d2)
+mean(d2)-mean(d1)
+sort(d1, decreasing = TRUE)
+d1 <- sort(d1, decreasing = TRUE)
+d1 <- d1[1:10]
+d2 <- sort(d2,decreasing = T)
+d2 <- d2[1:10]
+d3 <- c(d1,d2)
+d3
+
